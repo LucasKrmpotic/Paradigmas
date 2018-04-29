257760d8c730cc27eda636f866f6682c33f31af7
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2018-04-29 00:26:42 -0300

    Mas alta optima con un solo recorrido de lista para determinar el mas alto listo papu.

--- 1/black.pl
+++ 1/black.pl
@@ -163,6 +163,24 @@ fijate_mas_alto(Mano, ValorActual, ValorMasAlto):-
 mano_mas_alta(Mano, ValorMasAlto):-
 	fijate_mas_alto(Mano, 21, ValorMasAlto).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fijate_mas_alto_optimo([], MasAlto, MasAlto).
+fijate_mas_alto_optimo([NuevoValor|Resto], ValorMasAltoActual, ValorMasAlto):-
+	NuevoValor > ValorMasAltoActual,
+	NuevoValor < 22,
+	fijate_mas_alto_optimo(Resto, NuevoValor, ValorMasAlto),!.
+
+fijate_mas_alto_optimo([_|Resto], ValorMasAltoActual, ValorMasAlto):-
+	fijate_mas_alto_optimo(Resto, ValorMasAltoActual, ValorMasAlto).
+
+mano_mas_alta_optima(Mano, ValorMasAlto):-
+	findall(X, hand(Mano, X), ListaValores),
+	fijate_mas_alto_optimo(ListaValores, 0, ValorMasAlto).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 play(Jugador,_,_):-
 	not(twentyOne(Jugador)).
 
