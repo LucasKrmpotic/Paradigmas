7e71f51e55f6daee5d30ddc797f6f44f7029db2a
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2018-04-30 23:49:22 -0300

    Ya falta poco para que juegue piola

--- 1/black.pl
+++ 1/black.pl
@@ -178,20 +178,86 @@ mano_mas_alta_optima(Mano, ValorMasAlto):-
 	findall(X, hand(Mano, X), ListaValores),
 	fijate_mas_alto_optimo(ListaValores, 0, ValorMasAlto).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Implementacion de uston ss %
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+valor_uston_ss(a,-2).
+valor_uston_ss(2,2).
+valor_uston_ss(3,2).
+valor_uston_ss(4,2).
+valor_uston_ss(5,3).
+valor_uston_ss(6,2).
+valor_uston_ss(7,1).
+valor_uston_ss(8,0).
+valor_uston_ss(9,-2).
+valor_uston_ss(10,-2).
+valor_uston_ss(j,-2).
+valor_uston_ss(q,-2).
+valor_uston_ss(k,-2).
+
+contame_uston_ss([], ConteoActual, ConteoActual).
+
+contame_uston_ss(Cartas, ConteoActual, Conteo):-
+	[card(Carta,_)| Resto] = Cartas,
+	valor_uston_ss(Carta, ValorHuston),
+	ConteoNuevo is ConteoActual + ValorHuston,
+	contame_uston_ss(Resto, ConteoNuevo, Conteo).
+
+% La cantidad de barajas hacen que el conteo inicial sea de cantidadDeBarajas * -2
+% segun el algoritmo de conteo Huston SS.
+contar_uston_ss(Cartas, Barajas, Conteo):-
+	ValorInicial is Barajas * -2,
+	contame_uston_ss(Cartas, ValorInicial, Conteo).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-play(Jugador,_,_):-
-	not(twentyOne(Jugador)).
+es_as_once(Jugador,ValorMano):-
+	hand(Jugador, ValorMano),
+	ValorSoft is ValorMano - 10,
+	hand(Jugador, ValorMano),
+	!.
 
+
+% Hay posibilidad de baja si el conteo es bajo, eso significa que se jugaron bastantes altas.
+% Este valor es ajustable.
+posibilidadBaja(Conteo):-
+	Conteo < -2.				
+
+% Hay posibilidad de baja si el conteo es bajo, eso significa que se jugaron bastantes altas.
+% Este valor es ajustable.
+posibilidadAlta(Conteo):-
+	Conteo > 0.				
+
+
+% Juega si el valor de la mano del jugador no supero a la mano del crupier
 play(Jugador, Crupier, _):-
-    hand(Jugador, ValorManoJugador), 
-    hand(Crupier, ValorManoCrupier),
+    mano_mas_alta_optima(Jugador, ValorManoJugador), 
+    mano_mas_alta_optima(Crupier, ValorManoCrupier),
     not(ValorManoJugador > ValorManoCrupier) .
 
+% Ya sabemos que el valor de la mano supero a la del crupier.
+% Ahora hay que ver que no sean pocos puntos. 
+% Juega si el valor de la mano es menor a 11 pero no necesariamente si la mano mas alta es mayor a 18.
+% Para lo siguiente calcula las probabilidades en la tercera regla
 play(Jugador, _, _):- 
     hand(Jugador, ValorManoJugador),
-    hand(Jugador, ValorManoAux),
+    mano_mas_alta_optima(Jugador, ValorManoAux),
     ValorManoJugador < 11, 
     not(ValorManoAux > 18). 
 
+% Aca vemos cuantas posibilidades hay para pedir cartas. Hay que pensar que si tenes un
+% >18 soft podemos seguir pidiendo. Más si hay posibilidades de que hayan cartas bajas.
+play(Jugador, _, CartasJugadas):-
+	mano_mas_alta_optima(Jugador, ValorMano),	% Veo la mano mas cercana a 21. Veo si ese valor es
+	ValorMano > 18,
+	es_as_once(Jugador, ValorMano), 			% soft, o sea, tiene un A con valor 11. Cuento las
+	contar_uston_ss(CartasJugadas, 1, Conteo),	% cartas con uston ss y me fijo si hay la posibilidad que me toque 
+	posibilidadBaja(Conteo).					% una carta baja. Si es asi juego. 
+
+% Aca vemos la posibilidad de seguir sacando cartas para > 11 pero < 18. 
+play(Jugador, _, CartasJugadas):-
+	mano_mas_alta_optima(Jugador, ValorMano),	% Veo la mano mas cercana a 21. Veo si ese valor es
+	ValorMano < 16,
+	contar_uston_ss(CartasJugadas, 1, Conteo),	% es menor a 16 pido una carta siempre y cuando el valor de 
+	!posibilidadAlta(Conteo).					% el conteo me indique que no hay posibilidades de sacar una carta alta.
